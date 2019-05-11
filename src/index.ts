9583cb4d80ee9e628c562edf5bb1922904cf70f8
Eric Lewis <ericlewis777@gmail.com>
2019-05-11 00:48:23 -0400

    ci(package): update package.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { NativeModules } from 'react-native';
 const { RNWebImageLoader } = NativeModules;
 
 interface Spec {
-  readonly clearCache: (expiredOnly?: boolean) => Promise<void>;
+  readonly clearCache: (expiredOnly?: boolean) => Promise<null>;
   readonly getDiskSize: () => Promise<number>;
 }
 export default RNWebImageLoader as Spec;
